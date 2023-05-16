f4c59df4a042748e04a06a6e10a6a85c19d2feeb
Thomas <thomassvendal@gmail.com>
2023-05-16 13:57:16 +0200

    Added Ten drones that fly

--- src/simulation.rs
+++ src/simulation.rs
@@ -1,13 +1,15 @@
+use std::sync::{Arc, RwLock};
+
 pub struct World {
     pub drones: Vec<Drone>,
     pub emergency_beacons: Vec<EmergencyBeacon>,
 }
 
+pub struct Connection {}
+
 pub struct Drone {
     world: Arc<RwLock<World>>,
-    conntections: Vec<Conntection>,
+    conntections: Vec<Connection>,
 }
-
-pub struct Connection {}
 
 pub struct EmergencyBeacon {}
--- src/visualization.rs
+++ src/visualization.rs
@@ -1,10 +1,16 @@
-//! A simple 3D scene with light shining over a cube sitting on a plane.
+use std::f32::consts::PI;
 
 use bevy::prelude::*;
+use bevy_egui::EguiPlugin;
+
+const DRONE_ROWS: usize = 10;
+const DRONE_COLUMNS: usize = 10;
+const DRONE_SPACING: f32 = 4.0;
 
 pub fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(EguiPlugin)
         .add_startup_system(setup)
         .add_system(update_drones)
         .run();
@@ -16,8 +22,8 @@ pub struct Drone {}
 fn update_drones(mut drones: Query<&mut Transform, With<Drone>>) {
     for mut transform in drones.iter_mut() {
         transform.translation += Vec3::new(0.0, 0.0, 0.1);
-        if transform.translation.z > 5.0 {
-            transform.translation.z = -5.0;
+        if transform.translation.z > 200.0 {
+            transform.translation.z = -200.0;
         }
     }
 }
@@ -31,31 +37,55 @@ fn setup(
 ) {
     // plane
     commands.spawn(PbrBundle {
-        mesh: meshes.add(shape::Plane::from_size(5.0).into()),
+        mesh: meshes.add(shape::Plane::from_size(500.0).into()),
         material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
         ..default()
     });
 
     // light
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1500.0,
+    // commands.spawn(PointLightBundle {
+    //     point_light: PointLight {
+    //         intensity: 150000.0,
+    //         shadows_enabled: true,
+    //         ..default()
+    //     },
+    //     transform: Transform::from_xyz(4.0, 8.0, 4.0),
+    //     ..default()
+    // });
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
             shadows_enabled: true,
             ..default()
         },
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
-        ..default()
-    });
-    // camera
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
-    commands.spawn((
-        SceneBundle {
-            scene: asset_server.load("untitled.glb#Scene0"),
+        transform: Transform {
+            translation: Vec3::new(0.0, 2.0, 0.0),
+            rotation: Quat::from_rotation_x(-PI / 4.),
             ..default()
         },
-        Drone {},
-    ));
+        ..default()
+    });
+
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-20.0, 20.5, 50.0)
+            .looking_at(Vec3::new(0.0, 0.0, 20.0), Vec3::Y),
+        ..default()
+    });
+    for x in 0..DRONE_COLUMNS {
+        for z in 0..DRONE_ROWS {
+            commands.spawn((
+                SceneBundle {
+                    scene: asset_server.load("untitled.glb#Scene0"),
+                    transform: Transform::from_xyz(
+                        x as f32 * DRONE_SPACING,
+                        10.0,
+                        z as f32 * DRONE_SPACING,
+                    )
+                    .with_scale(Vec3::splat(0.1)),
+                    ..default()
+                },
+                Drone {},
+            ));
+        }
+    }
 }
