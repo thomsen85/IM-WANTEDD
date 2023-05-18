c270c530ae3d269bd6f0efe5528ef9225de77e4e
Thomas <thomassvendal@gmail.com>
2023-05-18 17:49:26 +0200

    Added emergency beacon

--- src/simulation.rs
+++ src/simulation.rs
@@ -10,10 +10,11 @@ mod ui;
 pub fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(scenery::SceneryPlugin)
         .add_plugin(ui::UIPlugin)
         .add_plugin(camera::CameraPlugin)
         .add_plugin(drones::DronesPlugin)
         .add_plugin(drone_connections::DroneConnectionsPlugin)
-        .add_plugin(scenery::SceneryPlugin)
+        .add_plugin(emergency_beacons::EmergencyBeaconsPlugin)
         .run();
 }
